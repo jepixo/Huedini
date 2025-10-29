2b3688ff13e526c2dfc7b02b22265143a9eb2b70
jepixo <joelsanjay7@gmail.com>
2025-10-29 05:52:55 +0530

    Initial commit

--- deploy.js
+++ deploy.js
@@ -1,9 +1,18 @@
-import ghpages from 'gh-pages';
+import ghpages from "gh-pages";
+import path from "path";
 
-ghpages.publish('dist', {
-  branch: 'gh-pages',
-  repo: 'https://github.com/jepixo/Huedini.git',
-  dotfiles: true
+const dir = path.resolve("dist");
+
+ghpages.publish(dir, {
+  branch: "gh-pages",
+  repo: "https://github.com/jepixo/Huedini.git",
+  dotfiles: true,
+  user: {
+    name: "jepixo",
+    email: "your-email@example.com"
+  },
+  message: "Deploying Huedini ✨",
+  cwd: process.cwd().replace(/\\/g, "/") // fix for Windows paths
 }, err => {
-  console.log(err ? `❌ Deploy failed: ${err}` : '✅ Deploy success!');
+  console.log(err ? `❌ Deploy failed:\n${err}` : "✅ Deploy success!");
 });
